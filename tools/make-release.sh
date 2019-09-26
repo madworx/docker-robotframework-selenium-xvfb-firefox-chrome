96133b53d7bd8c657c5c7b8cd353d065336cfb3c
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2019-09-26 12:31:18 +0200

    Travis 'after_success' doesn't break the build if the scripts fail. Move them to 'script' phase instead.

--- tools/make-release.sh
+++ tools/make-release.sh
@@ -2,21 +2,25 @@
 
 set -eE
 set -o pipefail
+set -x
 
 source <(curl 'https://raw.githubusercontent.com/madworx/cd-ci-glue/master/src/cd-ci-glue.bash')
 
+IMAGE_NAME="$(make -s get-image-name)"
 RELEASEVER="$(date +%Y%m)"
-RELEASENAME="$(make get-image-name):${RELEASEVER}"
+RELEASENAME="${IMAGE_NAME}:${RELEASEVER}"
 
-docker tag $(make get-image-name):latest "${RELEASENAME}"
-dockerhub_push_image "$(make get-image-name):latest"
+docker tag "${IMAGE_NAME}:latest" "${RELEASENAME}"
 dockerhub_push_image "${RELEASENAME}"
-
-WIKIDIR="$(github_wiki_prepare "madworx/docker-robotframework-selenium-xvfb-firefox-chrome")"
+dockerhub_push_image "${IMAGE_NAME}:latest"
 
 CODIR="$(pwd)"
+WIKIDIR="$(github_wiki_prepare "madworx/docker-robotframework-selenium-xvfb-firefox-chrome")"
 
 cd "${WIKIDIR}"
-docker inspect "${RELEASENAME}" | jq -r '.[0].Config.Labels' > "${WIKIDIR}/labels-${RELEASEVER}.json"
+docker inspect "${RELEASENAME}" | jq -r '.[0].Config.Labels' > "labels-${RELEASEVER}.json"
 cp "${CODIR}/README.md" "Home.md"
 ${CODIR}/tools/generate-version-wiki.py > "Releases.md"
+
+cd "${CODIR}"
+github_wiki_commit "${WIKIDIR}"
